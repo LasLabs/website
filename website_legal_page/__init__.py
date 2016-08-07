1e0a34c4f7a5e6b2c7a32bda53edb5dc17c37c23
Dave Lasley <dave@laslabs.com>
2016-08-07 14:18:50 -0700

    Break Build

--- website_legal_page/__init__.py
+++ website_legal_page/__init__.py
@@ -2,3 +2,5 @@
 # © 2015 Antiun Ingeniería S.L. (http://www.antiun.com)
 # © 2015 Antonio Espinosa <antonioea@antiun.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import no_exist
